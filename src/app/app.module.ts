423a001e3563c2fa1197f0ee6d8d45666d7adeb1
Kalikalyan Dash <kkdgrece@gmail.com>
2017-11-09 18:00:27 -0800

    new RecipeModule created and need to solve **is not an NgModule issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { RecipeListComponent } from './recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipe-item/recipe-item.component';
-import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
-import { RecipeComponent } from './recipe/recipe.component';
 import { ShoppingListEditComponent } from './shopping-list-edit/shopping-list-edit.component';
 import { IngredientComponent } from './ingredient/ingredient.component';
 import { DropdownDirective } from './directive/dropdown.directive';
 import { ShoppingListService } from './Services/shopping-list.service';
 import { AppRoutingModule} from './app-routing.module';
-import { RecipeStartComponent } from './recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
 import { RecipeService } from './Services/recipe.service';
 import {HttpModule} from '@angular/http';
 import {DataStorageService} from './Services/data.storage.service';
@@ -23,6 +17,7 @@ import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
 import {AuthService} from './auth/auth.service';
 import {AuthGuard} from './auth/auth-guard.service';
+import {RecipeModule} from './recipe/recipe.module';
 
 
 @NgModule({
@@ -30,15 +25,9 @@ import {AuthGuard} from './auth/auth-guard.service';
     AppComponent,
     HeaderComponent,
     ShoppingListComponent,
-    RecipeListComponent,
-    RecipeItemComponent,
-    RecipeDetailsComponent,
-    RecipeComponent,
     ShoppingListEditComponent,
     IngredientComponent,
     DropdownDirective,
-    RecipeStartComponent,
-    RecipeEditComponent,
     SignupComponent,
     SigninComponent
   ],
@@ -46,8 +35,8 @@ import {AuthGuard} from './auth/auth-guard.service';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule,
-    HttpModule
+    HttpModule,
+    RecipeModule
   ],
   providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
---
+++ src/app/recipe/recipe.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ReactiveFormsModule} from '@angular/forms';
+import {RecipeListComponent} from '../recipe-list/recipe-list.component';
+import {RecipeItemComponent} from '../recipe-item/recipe-item.component';
+import {RecipeDetailsComponent} from '../recipe-details/recipe-details.component';
+import {RecipeComponent} from '../recipe/recipe.component';
+import {RecipeStartComponent} from '../recipe-start/recipe-start.component';
+import {RecipeEditComponent} from '../recipe-edit/recipe-edit.component';
+
+
+NgModule({
+  declarations: [
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailsComponent,
+    RecipeComponent,
+    RecipeStartComponent,
+    RecipeEditComponent
+  ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ]
+})
+
+
+export class RecipeModule {
+
+}
