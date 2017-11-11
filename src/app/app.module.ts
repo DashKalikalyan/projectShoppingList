1538f7fa386e2e4fd58fee735f250647d90ffdf5
Kalikalyan Dash <kkdgrece@gmail.com>
2017-11-10 16:38:12 -0800

    Auth Module separated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -10,31 +9,28 @@ import { AppRoutingModule} from './app-routing.module';
 import { RecipeService } from './Services/recipe.service';
 import {HttpModule} from '@angular/http';
 import {DataStorageService} from './Services/data.storage.service';
-import { SignupComponent } from './auth/signup/signup.component';
-import { SigninComponent } from './auth/signin/signin.component';
 import {AuthService} from './auth/auth.service';
 import {AuthGuard} from './auth/auth-guard.service';
 import {RecipeModule} from './recipe/recipe.module';
 import {SharedModule} from './directive/shared.module';
 import {ShoppingListModule} from './shopping-list/shopping-list.module';
+import {AuthModule} from './auth/auth.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    IngredientComponent,
-    SignupComponent,
-    SigninComponent
+    IngredientComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
     HttpModule,
     RecipeModule,
     SharedModule,
-    ShoppingListModule
+    ShoppingListModule,
+    AuthModule
   ],
   providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
